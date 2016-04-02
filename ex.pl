dba69b48390d1fe98e5987287cc59bda3d5c85b6
Candy <candy.goodison@redbubble.com>
2016-04-02 22:41:37 +1100

    Storing the extracted values in an array-hash
    The content Extractor subroutine returns key->value pairs from the email
    json which are then stored in an array->hash structure. Each element of
    the array stores a hash representing one email.

--- ex.pl
+++ ex.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use LWP::Simple;
 
 open my $openfile, "emails.json" or die "Error opening file: $!\n";
 
@@ -9,29 +8,72 @@ my %kv_blocks;
 
 my $line_num = 0;
 
+my @emails; # An array of emails
+my $emailCount = -1; #A count to track the position in the email array
+
 # open file and read each line
 while (<$openfile>)
-{
+{	
 	# debug: line number
 	$line_num++;
 	
 	# remove leading/trailing white-spaces
 	$_ =~ s/^\s+|\s+$//g;
 	
-	#print "$line_num:$_\n";
-	
 	# if a new block we prepare for key/value pairs
 	if ($_ =~ m/{/)
 	{
+		$emailCount++;
 		next;
 	}
+	
+	my $existingHash = $emails[$emailCount];
+	my $newHash = contentExtractor();
+	
+	if($existingHash && $newHash)
+	{
+		my$mergedHash = {%$existingHash, %$newHash};
+		$emails[$emailCount] = $mergedHash;
+	}
+	elsif($newHash)
+	{
+		$emails[$emailCount] = $newHash;
+	}
+}
+
+#print data
+print "Number of emails: ";
+print (scalar @emails);
+print "\n-------------------\n";
+for (my $i = 0; $i < (scalar @emails); $i++) 
+{
+	my $hash = $emails[$i];
+	if ($hash) 
+	{
+		my @keys = keys $hash;
+	
+		for (my $j = 0; $j < (scalar @keys); $j++) 
+		{
+			print $keys[$j];
+			print " : ";
+			print $hash->{$keys[$j]};
+			print "\n";
+		}
+	
+		print "-------------------\n";
+	}
+	
+}
+#end print data
+
+close $openfile;
+
+#Subroutine that returns a hash from one 
+sub contentExtractor {
 	
 	# if within the key/value portion - we preserve contents
 	if ($_ =~ /^"/)
 	{
-		# debug: print string
-		print "$_\n";		
-		
 		# new variables
 		my $extract_key;
 		my $extract_value;
@@ -45,43 +87,20 @@ while (<$openfile>)
 		    # assign key to extracted data ($1 from stack)
 		    # http://stackoverflow.com/questions/1485046/how-can-i-extract-a-substring-enclosed-in-double-quotes-in-perl
 		    $extract_key = $1;
-		    
 		    # same thing here - extract and assign
 		    $_ =~ s/"(.*?)"//s;
 		    $extract_value = $1;
 		}
-		
-		# debug: perform web-request with timezone
-		if ($extract_key eq "timeZone")
-		{
-			my $contents = get("http://api.timezonedb.com/?key=EHUCC69JBOJT&zone=$extract_value");
-			print $contents;
-		}
-		
-		print "->[$extract_key] = $extract_value\n";
-		
-		#my @kv_split = split(/:/, $_);
-		#my $kv_combine;
-		#
-		#my $arraylen = $#kv_split;
-		#	
-		#if ($arraylen != 2)
-		#{
-		#	next;
-		#}
-		#
-		#for (my $i = 0; $i <= $arraylen; $i++) {
-		#	$kv_split[$i] =~ s/^\s+|\s+$//g; 	# remove whitespaces
-		#	$kv_split[$i] =~ s/,+$//g;		# remove ending commas for values
-		#}
-		#
-		#$kv_blocks{$kv_split[0]} = $kv_split[1];
-		#
-		#my $kv_key = $kv_split[0];
-		#my $kv_value = $kv_blocks{$kv_split[0]};
-		#
-		#print "KV_HASH->$kv_key:$kv_value\n";
+		my $existingHash->{$extract_key} = $extract_value;
+		return $existingHash;
 	}
 }
 
-close $openfile;
+
+
+# # debug: perform web-request with timezone
+		# if ($extract_key eq "timeZone")
+		# {
+			# my $contents = get("http://api.timezonedb.com/?key=EHUCC69JBOJT&zone=$extract_value");
+			# print $contents;
+		# }
